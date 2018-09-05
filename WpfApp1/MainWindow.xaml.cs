0643c92da55d81bb25bc6460f3e71382eed3e0fe
headdyx <31770714+headdyx@users.noreply.github.com>
2018-09-06 01:14:23 +0200

    Projektdateien hinzufügen.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public String url = "https://github.com/cefsharp/CefSharp/wiki/Frequently-asked-questions";
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.url = MyTextBox.Text;
+            CefSharp.Address = this.url;
+        }
+    }
+}
